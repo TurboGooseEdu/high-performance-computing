d6c53d8501b2b1568e94d7f62b7f343697bc07d2
ilakonovalov <antonpuckich@gmail.com>
2023-11-17 22:06:37 +0300

    Add paralleling pragmas

--- task1/program.cpp
+++ task1/program.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 
-#define N 2
+#define N 1000
 
 
 double** init_matrix() {
@@ -64,7 +64,7 @@ double** matmul(double** a, double** b)
 {
     double** res = init_matrix();
     int i, j, k;
-    // #pragma omp parallel for private(i,j,k)
+    #pragma omp parallel for private(i,j,k) shared(a,b,res)
     for (i = 0; i < N; ++i) {
         for (j = 0; j < N; ++j) {
             for (k = 0; k < N; ++k) {
@@ -76,7 +76,7 @@ double** matmul(double** a, double** b)
 } 
 
 
-double** deg(double** matrix, int deg) {
+double** pow(double** matrix, int deg) {
     double** mulcopy = copy_matrix(matrix);
     for (int i = 0; i < deg - 1; i++) {
         mulcopy = matmul(mulcopy, matrix);
@@ -86,8 +86,10 @@ double** deg(double** matrix, int deg) {
 
 double** sum(double** a, double** b) {
     double** res = init_matrix();
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    int i, j;
+    #pragma omp parallel for private(i,j) shared(a,b,res)
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             res[i][j] = a[i][j] + b[i][j];
         }
     }
@@ -96,30 +98,49 @@ double** sum(double** a, double** b) {
 
 double trace_of_sum(double** a, double** b) {
     double trace = 0;
+    #pragma omp parallel for reduction (+:trace)
     for (int i = 0; i < N; i++) {
         trace += a[i][i] + b[i][i];
     }
     return trace;
 }
 
-// A = B^4 + C^4 + Tr(B^3 + C^3)E
+double calculate(double** B, double** C) {
+    double start = omp_get_wtime();
 
-int main()
-{
-    double** B = generate_random_square_matrix(1);
-    double** C = generate_random_square_matrix(2);
-    
-    double** B_3 = deg(B, 3);
-    double** C_3 = deg(C, 3);
+    double** B_3 = pow(B, 3);
+    double** C_3 = pow(C, 3);
 
     double** D = get_matrix_of_numbers(trace_of_sum(B_3, C_3));
 
     double** B_4 = matmul(B_3, B);
     double** C_4 = matmul(C_3, C);
 
+
     double** A = sum(sum(B_4, C_4), D);
 
-    print_matrix(A);
+    double end = omp_get_wtime();
+    return end - start;
+}
 
+// A = B^4 + C^4 + Tr(B^3 + C^3)E
+
+int main()
+{
+    int threads = 1;
+
+    double prev_result = 0;
+    double cur_result = 999999999;
+
+    double** B = generate_random_square_matrix(1);
+    double** C = generate_random_square_matrix(2);
+    
+    do {
+        omp_set_num_threads(threads);
+        prev_result = cur_result;
+        cur_result = calculate(B, C);
+        printf("Threads: %d; Elapsed time: %f\n", threads, cur_result);
+        threads++;
+    } while (cur_result < prev_result);
     return 0;
 }
