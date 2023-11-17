eedb4fcc8590afc3b6548e174c6d7eddb3af9cbe
ilakonovalov <antonpuckich@gmail.com>
2023-11-17 20:56:18 +0300

    Now it is validated

--- task1/program.cpp
+++ task1/program.cpp
@@ -1,6 +1,5 @@
 #include <omp.h>
 #include <iostream>
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -23,7 +22,7 @@ double** copy_matrix(double** matrix) {
             copy[i][j] = matrix[i][j];
         }
     }
-    return matrix;
+    return copy;
 }
 
 void print_matrix(double** matrix) {
@@ -36,6 +35,17 @@ void print_matrix(double** matrix) {
     std::cout << std::endl;
 }
 
+double** get_matrix_of_numbers(double num) {
+    double** matrix = new double*[N];
+    for (int i = 0; i < N; ++i) {
+        matrix[i] = new double[N];
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = num;
+        }
+    }
+    return matrix;
+}
+
 
 double** generate_random_square_matrix(unsigned int seed) {
     std::srand(seed);
@@ -46,7 +56,6 @@ double** generate_random_square_matrix(unsigned int seed) {
             matrix[i][j] = std::rand() % 10;
         }
     }
-    print_matrix(matrix);
     return matrix;
 }
 
@@ -71,7 +80,6 @@ double** deg(double** matrix, int deg) {
     double** mulcopy = copy_matrix(matrix);
     for (int i = 0; i < deg - 1; i++) {
         mulcopy = matmul(mulcopy, matrix);
-        print_matrix(mulcopy);
     }
     return mulcopy;
 }
@@ -86,38 +94,32 @@ double** sum(double** a, double** b) {
     return res;
 }
 
-double** composite_calc(double** B_3, double** C_3) {
-    double** res = init_matrix();
+double trace_of_sum(double** a, double** b) {
+    double trace = 0;
     for (int i = 0; i < N; i++) {
-        int C = B_3[i][i] + B_3[i][i];
-        for (int j = 0; j < N; j++) {
-            res[i][j] = C;
-        }
+        trace += a[i][i] + b[i][i];
     }
-    return res;
+    return trace;
 }
 
 // A = B^4 + C^4 + Tr(B^3 + C^3)E
 
 int main()
 {
-    omp_set_num_threads(omp_get_num_procs());
-
     double** B = generate_random_square_matrix(1);
     double** C = generate_random_square_matrix(2);
-
+    
     double** B_3 = deg(B, 3);
     double** C_3 = deg(C, 3);
-    double** D = composite_calc(B_3, C_3);
+
+    double** D = get_matrix_of_numbers(trace_of_sum(B_3, C_3));
 
     double** B_4 = matmul(B_3, B);
     double** C_4 = matmul(C_3, C);
 
     double** A = sum(sum(B_4, C_4), D);
 
-    print_matrix(B_4);
-    print_matrix(C_4);
-
+    print_matrix(A);
 
     return 0;
 }
