fbb3ab241ed789ed7950f1fe2a89723897478b72
ilakonovalov <antonpuckich@gmail.com>
2023-12-14 15:01:16 +0300

    Implement run script for entire experiment

--- src/mpi/run.sh
+++ src/mpi/run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-rm results.txt
+procs=(2 4 8)
+dims=(160 640 1280)
+
+output_file=results.txt
 prog_name=parallel
 
-mpic++ "$prog_name".cpp -o "$prog_name".o -w
-mpirun -np 2 "$prog_name".o 
+rm "$output_file"
+
+mpic++  -w "$prog_name".cpp -o "$prog_name".o
+
+for dim in "${dims[@]}"; do
+    for proc in "${procs[@]}"; do
+        echo "-- Start experiment with N=$dim and $proc processes"
+        mpirun -np "${proc}" "$prog_name".o "${dim}"
+        echo -e "-- Completed!\n"
+    done
+done
+
 rm "$prog_name".o
